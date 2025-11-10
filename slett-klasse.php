f32c538d967284214d49753db6cb81e1ae1510f4
Gringofulafulanito <el.vegar.eriksen@gmail.com>
2025-11-10 15:35:36 +0100

    hahahahaah

--- slett-klasse.php
+++ slett-klasse.php
@@ -14,6 +14,7 @@
 <?php print("<option value=''>velg klasse</option>");
   include("dynamiske-funksjoner.php"); listeboksKlasse();?> 
   </select>  <br/> 
+  rtfyguhij
   <input type="submit" value="Slett klasse" name="slettklasseKnapp" id="slettklasseKnapp" /> 
 </form>
 
