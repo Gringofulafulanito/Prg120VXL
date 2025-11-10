f7e5a99f17921b2455158fca90138640c57a0c49
Gringofulafulanito <el.vegar.eriksen@gmail.com>
2025-11-10 13:59:26 +0100

    message141

---
+++ dynamiske-funksjoner.php
@@ -0,0 +1,50 @@
+<?php
+function listeboksKlasse()
+{
+  include("db-tilkobling.php");  /* tilkobling til database-server og valg av database utført */
+      
+  $sqlSetning="SELECT * FROM klasse ORDER BY klasse;";
+  $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen"); 
+    /* SQL-setning sendt til database-serveren */
+	
+  $antallRader=mysqli_num_rows($sqlResultat);  /* antall rader i resultatet beregnet */
+
+  for ($r=1;$r<=$antallRader;$r++)
+    {
+      $rad=mysqli_fetch_array($sqlResultat);  /* ny rad hentet fra spørringsresultatet */
+      $klasse=$rad["klassekode"]; 
+      $klassenavn=$rad["klassenavn"];
+      $studiumkode=$rad("studiumkode");
+
+      print("<option value='$klasse'>$klassenavn $studiumkode</option>");  /* ny verdi i listeboksen laget */
+    }
+}
+
+
+
+
+function listeboksstudent()
+{
+  include("db-tilkobling.php");  /* tilkobling til database-server og valg av database utført */
+      
+  $sqlSetning="SELECT * FROM student ORDER BY student;";
+  $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen"); 
+    /* SQL-setning sendt til database-serveren */
+	
+  $antallRader=mysqli_num_rows($sqlResultat);  /* antall rader i resultatet beregnet */
+
+  for ($r=1;$r<=$antallRader;$r++)
+    {
+      $rad=mysqli_fetch_array($sqlResultat);  /* ny rad hentet fra spørringsresultatet */
+      $student=$$rad["student"]; 
+      $brukernavn=$rad["brukernavn"];
+      
+
+      print("<option value='$student'>$brukernavn</option>");  /* ny verdi i listeboksen laget */
+    }
+}
+
+
+
+
+?>
