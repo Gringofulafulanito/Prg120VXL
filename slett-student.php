c9e318620a2f3fd7e912101ce53093375f9c9a08
Gringofulafulanito <el.vegar.eriksen@gmail.com>
2025-11-10 12:33:26 +0100

    message11

--- slett-student.php
+++ slett-student.php
@@ -10,8 +10,8 @@
 <h3>Slett student</h3>
 
 <form method="post" action="" id="slettStudentSkjema" name="slettStudentSkjema" onSubmit="return bekreft()">
-  Postnr <input type="text" id="student" name="student" required /> <br/>
-  <input type="submit" value="Slett student" name="slettStudentKnapp" id="slettPoststedKnapp" /> 
+  Student <input type="text" id="student" name="student" required /> <br/>
+  <input type="submit" value="Slett student" name="slettStudentKnapp" id="slettStudentKnapp" /> 
 </form>
 
 <?php
